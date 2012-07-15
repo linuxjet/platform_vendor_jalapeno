1e4ca83203b5ee8bdc8781cc02970bb504c5c998
James Peterson <James.Peterson@linuxjet.com>
2012-07-15 17:27:43 -0500

    Started vendor profile based on Peter Alfonso's I wanted to start working on a few custom ideas. Pete has done great work so was a good place to start

--- products/common.mk
+++ products/common.mk
@@ -1,10 +1,10 @@
 # Generic product
-PRODUCT_NAME := pete
-PRODUCT_BRAND := pete
+PRODUCT_NAME := jalapeno
+PRODUCT_BRAND := jalapeno
 PRODUCT_DEVICE := generic
 
 # Common overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/pete/overlay/common
+PRODUCT_PACKAGE_OVERLAYS += vendor/jalapeno/overlay/common
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
@@ -18,58 +18,70 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Blobs common to all devices
 PRODUCT_COPY_FILES += \
-    vendor/pete/proprietary/common/app/Books.apk:system/app/Books.apk \
-    vendor/pete/proprietary/common/app/Calendar.apk:system/app/Calendar.apk \
-    vendor/pete/proprietary/common/app/ChromeBookmarksSyncAdapter.apk:system/app/ChromeBookmarksSyncAdapter.apk \
-    vendor/pete/proprietary/common/app/Gmail.apk:system/app/Gmail.apk \
-    vendor/pete/proprietary/common/app/GoogleBackupTransport.apk:system/app/GoogleBackupTransport.apk \
-    vendor/pete/proprietary/common/app/GoogleContactsSyncAdapter.apk:system/app/GoogleContactsSyncAdapter.apk \
-    vendor/pete/proprietary/common/app/GoogleFeedback.apk:system/app/GoogleFeedback.apk \
-    vendor/pete/proprietary/common/app/GoogleLoginService.apk:system/app/GoogleLoginService.apk \
-    vendor/pete/proprietary/common/app/GooglePartnerSetup.apk:system/app/GooglePartnerSetup.apk \
-    vendor/pete/proprietary/common/app/GoogleServicesFramework.apk:system/app/GoogleServicesFramework.apk \
-    vendor/pete/proprietary/common/app/GoogleTTS.apk:system/app/GoogleTTS.apk \
-    vendor/pete/proprietary/common/app/LatinIMEDictionaryPack.apk:system/app/LatinIMEDictionaryPack.apk \
-    vendor/pete/proprietary/common/app/Maps.apk:system/app/Maps.apk \
-    vendor/pete/proprietary/common/app/MediaUploader.apk:system/app/MediaUploader.apk \
-    vendor/pete/proprietary/common/app/NetworkLocation.apk:system/app/NetworkLocation.apk \
-    vendor/pete/proprietary/common/app/OneTimeInitializer.apk:system/app/OneTimeInitializer.apk \
-    vendor/pete/proprietary/common/app/Phonesky.apk:system/app/Phonesky.apk \
-    vendor/pete/proprietary/common/app/QuickSearchBox.apk:system/app/QuickSearchBox.apk \
-    vendor/pete/proprietary/common/app/Street.apk:system/app/Street.apk \
-    vendor/pete/proprietary/common/app/Superuser.apk:system/app/Superuser.apk \
-    vendor/pete/proprietary/common/app/Talk.apk:system/app/Talk.apk \
-    vendor/pete/proprietary/common/app/talkback.apk:system/app/talkback.apk \
-    vendor/pete/proprietary/common/etc/contributors.css:system/etc/contributors.css \
-    vendor/pete/proprietary/common/etc/resolv.conf:system/etc/resolv.conf \
-    vendor/pete/proprietary/common/etc/permissions/com.google.android.maps.xml:system/etc/permissions/com.google.android.maps.xml \
-    vendor/pete/proprietary/common/etc/permissions/features.xml:system/etc/permissions/features.xml \
-    vendor/pete/proprietary/common/etc/updatecmds/google_generic_update.txt:system/etc/updatecmds/google_generic_update.txt \
-    vendor/pete/proprietary/common/framework/com.google.android.maps.jar:system/framework/com.google.android.maps.jar \
-    vendor/pete/proprietary/common/lib/libflint_engine_jni_api.so:system/lib/libflint_engine_jni_api.so \
-    vendor/pete/proprietary/common/lib/libpicowrapper.so:system/lib/libpicowrapper.so \
-    vendor/pete/proprietary/common/lib/libspeexwrapper.so:system/lib/libspeexwrapper.so \
-    vendor/pete/proprietary/common/lib/libvideochat_jni.so:system/lib/libvideochat_jni.so \
-    vendor/pete/proprietary/common/lib/libvideochat_stabilize.so:system/lib/libvideochat_stabilize.so \
-    vendor/pete/proprietary/common/lib/libvoicesearch.so:system/lib/libvoicesearch.so \
-    vendor/pete/proprietary/common/xbin/su:system/xbin/su
+    vendor/jalapeno/proprietary/common/app/BackupRestoreConfirmation.apk:system/app/BackupRestoreConfirmation.apk \
+    vendor/jalapeno/proprietary/common/app/Calendar.apk:system/app/Calendar.apk \
+    vendor/jalapeno/proprietary/common/app/CalendarProvider.apk:system/app/CalendarProvider.apk \
+    vendor/jalapeno/proprietary/common/app/Chrome.apk:system/app/Chrome.apk \
+    vendor/jalapeno/proprietary/common/app/DefaultContainerService.apk:system/app/DefaultContainerService.apk \
+    vendor/jalapeno/proprietary/common/app/FaceLock.apk:system/app/FaceLock.apk \
+    vendor/jalapeno/proprietary/common/app/Gallery2.apk:system/app/Gallery2.apk \
+    vendor/jalapeno/proprietary/common/app/GenieWidget.apk:system/app/GenieWidget.apk \
+    vendor/jalapeno/proprietary/common/app/Gmail.apk:system/app/Gmail.apk \
+    vendor/jalapeno/proprietary/common/app/GmsCore.apk:system/app/GmsCore.apk \
+    vendor/jalapeno/proprietary/common/app/GoogleBackupTransport.apk:system/app/GoogleBackupTransport.apk \
+    vendor/jalapeno/proprietary/common/app/GoogleContactsSyncAdapter.apk:system/app/GoogleContactsSyncAdapter.apk \
+    vendor/jalapeno/proprietary/common/app/GoogleEars.apk:system/app/GoogleEars.apk \
+    vendor/jalapeno/proprietary/common/app/GoogleFeedback.apk:system/app/GoogleFeedback.apk \
+    vendor/jalapeno/proprietary/common/app/GoogleLoginService.apk:system/app/GoogleLoginService.apk \
+    vendor/jalapeno/proprietary/common/app/GooglePartnerSetup.apk:system/app/GooglePartnerSetup.apk \
+    vendor/jalapeno/proprietary/common/app/GoogleServicesFramework.apk:system/app/GoogleServicesFramework.apk \
+    vendor/jalapeno/proprietary/common/app/GoogleTTS.apk:system/app/GoogleTTS.apk \
+    vendor/jalapeno/proprietary/common/app/LatinIMEDictionaryPack.apk:system/app/LatinIMEDictionaryPack.apk \
+    vendor/jalapeno/proprietary/common/app/Maps.apk:system/app/Maps.apk \
+    vendor/jalapeno/proprietary/common/app/MediaUploader.apk:system/app/MediaUploader.apk \
+    vendor/jalapeno/proprietary/common/app/Music.apk:system/app/Music.apk \
+    vendor/jalapeno/proprietary/common/app/NetworkLocation.apk:system/app/NetworkLocation.apk \
+    vendor/jalapeno/proprietary/common/app/OneTimeInitializer.apk:system/app/OneTimeInitializer.apk \
+    vendor/jalapeno/proprietary/common/app/Phonesky.apk:system/app/Phonesky.apk \
+    vendor/jalapeno/proprietary/common/app/SettingsProvider.apk:system/app/SettingsProvider.apk \
+    vendor/jalapeno/proprietary/common/app/SetupWizard.apk:system/app/SetupWizard.apk \
+    vendor/jalapeno/proprietary/common/app/SharedStorageBackup.apk:system/app/SharedStorageBackup.apk \
+    vendor/jalapeno/proprietary/common/app/Street.apk:system/app/Street.apk \
+    vendor/jalapeno/proprietary/common/app/Superuser.apk:system/app/Superuser.apk \
+    vendor/jalapeno/proprietary/common/app/SystemUI.apk:system/app/SystemUI.apk \
+    vendor/jalapeno/proprietary/common/app/Talk.apk:system/app/Talk.apk \
+    vendor/jalapeno/proprietary/common/app/Velvet.apk:system/app/Velvet.apk \
+    vendor/jalapeno/proprietary/common/app/VisualizationWallpapers.apk:system/app/VisualizationWallpapers.apk \
+    vendor/jalapeno/proprietary/common/app/VpnDialogs.apk:system/app/VpnDialogs.apk \
+    vendor/jalapeno/proprietary/common/app/Wallet.apk:system/app/Wallet.apk \
+    vendor/jalapeno/proprietary/common/lib/libchromeview.so:system/lib/libchromeview.so \
+    vendor/jalapeno/proprietary/common/lib/libgoogle_recognizer_jni.so:system/lib/libgoogle_recognizer_jni.so \
+    vendor/jalapeno/proprietary/common/lib/libjni_eglfence.so:system/lib/libjni_eglfence.so \
+    vendor/jalapeno/proprietary/common/lib/libjni_mosaic.so:system/lib/libjni_mosaic.so \
+    vendor/jalapeno/proprietary/common/lib/libspeexwrapper.so:system/lib/libspeexwrapper.so \
+    vendor/jalapeno/proprietary/common/lib/libttscompat.so:system/lib/libttscompat.so \
+    vendor/jalapeno/proprietary/common/lib/libttspico.so:system/lib/libttspico.so \
+    vendor/jalapeno/proprietary/common/lib/libvideochat_jni.so:system/lib/libvideochat_jni.so \
+    vendor/jalapeno/proprietary/common/lib/libvideochat_stabilize.so:system/lib/libvideochat_stabilize.so \
+    vendor/jalapeno/proprietary/common/lib/libvoicesearch.so:system/lib/libvoicesearch.so \
+    vendor/jalapeno/proprietary/common/lib/libvorbisencoder.so:system/lib/libvorbisencoder.so \
+    vendor/jalapeno/proprietary/common/xbin/busybox:system/xbin/busybox \
+    vendor/jalapeno/proprietary/common/xbin/su:system/xbin/su \
+    vendor/jalapeno/proprietary/common/xbin/sysro:system/xbin/sysro \
+    vendor/jalapeno/proprietary/common/xbin/sysrw:system/xbin/sysrw
 
 ifeq ($(TARGET_BUILD_VARIANT),user)
 # Blobs common to all devices except emulator
 PRODUCT_COPY_FILES += \
-    vendor/pete/proprietary/common/app/Gallery2.apk:system/app/Gallery2.apk \
-    vendor/pete/proprietary/common/app/Music.apk:system/app/Music.apk \
-    vendor/pete/proprietary/common/app/Videos.apk:system/app/Videos.apk \
-    vendor/pete/proprietary/common/app/VoiceSearch.apk:system/app/VoiceSearch.apk \
-    vendor/pete/proprietary/common/app/YouTube.apk:system/app/YouTube.apk
+    vendor/jalapeno/proprietary/common/app/Gallery2.apk:system/app/Gallery2.apk
 endif
 
-ifneq ($(filter pete_crespo pete_crespo4g pete_maguro pete_toro pete_toroplus,$(TARGET_PRODUCT)),)
+ifneq ($(filter jalapeno_toro,$(TARGET_PRODUCT)),)
 # Blobs common to all devices except emulator and tablets
 PRODUCT_COPY_FILES += \
-    vendor/pete/proprietary/common/app/CarHome.apk:system/app/CarHome.apk \
-    vendor/pete/proprietary/common/app/GenieWidget.apk:system/app/GenieWidget.apk \
-    vendor/pete/proprietary/common/app/SetupWizard.apk:system/app/SetupWizard.apk
+    vendor/jalapeno/proprietary/common/app/CarHome.apk:system/app/CarHome.apk \
+    vendor/jalapeno/proprietary/common/app/GenieWidget.apk:system/app/GenieWidget.apk \
+    vendor/jalapeno/proprietary/common/app/SetupWizard.apk:system/app/SetupWizard.apk
 endif
 
 # Enable SIP+VoIP on all targets
--- products/toro.mk
+++ products/toro.mk
@@ -2,39 +2,41 @@
 $(call inherit-product, device/samsung/toro/full_toro.mk)
 
 # Inherit common product files.
-$(call inherit-product, vendor/pete/products/common.mk)
+$(call inherit-product, vendor/jalapeno/products/common.mk)
 
 # Setup device specific product configuration.
-PRODUCT_NAME := pete_toro
+PRODUCT_NAME := jalapeno_toro
 PRODUCT_BRAND := google
 PRODUCT_DEVICE := toro
 PRODUCT_MODEL := Galaxy Nexus
 PRODUCT_MANUFACTURER := samsung
 
-PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=mysid BUILD_FINGERPRINT=google/mysid/toro:4.0.4/IMM76K/336647:user/release-keys PRIVATE_BUILD_DESC="mysid-user 4.0.4 IMM76K 336647 release-keys" BUILD_NUMBER=336647
+PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=mysid BUILD_FINGERPRINT=google/mysid/toro:4.1.1/JRO03C/336647:user/release-keys PRIVATE_BUILD_DESC="mysid-user 4.1.1 JRO03C 336647 release-keys" BUILD_NUMBER=336647
 
 # Inherit common build.prop overrides
--include vendor/pete/products/common_versions.mk
+-include vendor/jalapeno/products/common_versions.mk
+
+PRODUCT_PACKAGES +=
 
 # Extra toro overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/pete/overlay/toro
+PRODUCT_PACKAGE_OVERLAYS += vendor/jalapeno/overlay/toro
 
 # Copy toro specific prebuilt files
 PRODUCT_COPY_FILES +=  \
-    vendor/pete/proprietary/tuna/app/Thinkfree.apk:system/app/Thinkfree.apk \
-    vendor/pete/proprietary/tuna/media/bootanimation.zip:system/media/bootanimation.zip \
-    vendor/pete/proprietary/tuna/media/audio/notifications/Nexus.mp3:system/media/audio/notifications/Nexus.mp3 \
-    vendor/pete/proprietary/common/app/Microbes.apk:system/app/Microbes.apk \
-    vendor/pete/proprietary/common/app/Wallet.apk:system/app/Wallet.apk \
-    vendor/pete/proprietary/common/lib/libmicrobes_jni.so:system/lib/libmicrobes_jni.so
+    vendor/jalapeno/proprietary/tuna/app/Thinkfree.apk:system/app/Thinkfree.apk \
+    vendor/jalapeno/proprietary/tuna/media/bootanimation.zip:system/media/bootanimation.zip \
+    vendor/jalapeno/proprietary/tuna/media/audio/notifications/Nexus.mp3:system/media/audio/notifications/Nexus.mp3 \
+    vendor/jalapeno/proprietary/common/app/Microbes.apk:system/app/Microbes.apk \
+    vendor/jalapeno/proprietary/common/app/Wallet.apk:system/app/Wallet.apk \
+    vendor/jalapeno/proprietary/common/lib/libmicrobes_jni.so:system/lib/libmicrobes_jni.so
 
 # Inherit media effect blobs
--include vendor/pete/products/common_media_effects.mk
+-include vendor/jalapeno/products/common_media_effects.mk
 
 # Inherit Face lock security blobs
--include vendor/pete/products/common_facelock.mk
+-include vendor/jalapeno/products/common_facelock.mk
 
 # Inherit drm blobs
--include vendor/pete/products/common_drm_phone.mk
+-include vendor/jalapeno/products/common_drm_phone.mk
 
 
